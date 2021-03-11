96370f8898bf94bc9be1a6831c2eab6efcda7843
Lyten <g.m.v.s.03@gmail.com>
2021-03-11 13:53:49 +0200

    lab9 modal-window

--- java/lab8/app.js
+++ java/lab8/app.js
@@ -7,7 +7,6 @@ let t = z = 0
 
 btn.addEventListener("click", function(){
     t = h*((Math.sin(h) * Math.sin(h)) + c * (Math.cos(k) * Math.cos(k)));
-    
     z = (h * Math.log(h**k + c) + 3);
     alert("Your answers: t = "+ String(t) + " and z = " + String(z)) 
 })
